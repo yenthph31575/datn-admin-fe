ae9002c652b6c9844218a6d6a7047e548382ec88
lqhuy2004 <huyquang1256@gmail.com>
2025-12-04 18:39:17 +0700

    update

--- src/api/product/requests.ts
+++ src/api/product/requests.ts
@@ -37,7 +37,7 @@ export const updateProduct = async ({
 }): Promise<IProduct> => {
   const { data } = await client({
     url: `/api/admin/products/${id}`,
-    method: 'PATCH',
+    method: 'DELETE',
     data: formData,
   });
   return data?.data;
